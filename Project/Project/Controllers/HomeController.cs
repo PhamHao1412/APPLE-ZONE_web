3d239aca0d41d5be34643f572cef94cdbf16914e
PhamHao1412 <87088426+PhamHao1412@users.noreply.github.com>
2023-04-25 09:33:24 +0700

    initial commit

---
+++ Project/Project/Controllers/HomeController.cs
@@ -0,0 +1,66 @@
+﻿using Project.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PagedList;
+namespace Project.Controllers
+{
+    public class HomeController : Controller
+    {
+        AppleDataDataContext db = new AppleDataDataContext();
+        public ActionResult Index()
+        {
+            ViewBag.iPhone = db.Items.Where(s => s.maloai == 1).Take(4).ToList();
+            ViewBag.mac = db.Items.Where(s => s.maloai == 2).Take(4).ToList();
+
+            return View(db.Items.ToList());
+        }
+
+        public ActionResult Store()
+        {
+            ViewBag.iPhone = db.Items.Where(s => s.maloai == 1).Take(4).ToList();
+            ViewBag.mac = db.Items.Where(s => s.maloai == 2).Take(4).ToList();
+            ViewBag.iPad = db.Items.Where(s => s.maloai == 3).Take(4).ToList();
+            ViewBag.Watch = db.Items.Where(s => s.maloai == 4).Take(4).ToList();
+            ViewBag.Airpods = db.Items.Where(s => s.maloai == 5).Take(4).ToList();
+            return View();
+        }    public ActionResult Product_Details(int id)
+        {
+            if (id == null)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                var item = db.Items.FirstOrDefault(d =>d.ma==id);
+                ViewBag.HinhAnh = db.HinhAnhs.Where(s => s.ma == id).ToList();
+                return View(item);
+            }
+        }
+       
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [ChildActionOnly]
+        public ActionResult MenuView()
+        {
+            var menu = db.Loais.ToList();
+            return PartialView(menu);
+        }
+
+    }
+}
---
+++ Project/Project/Models/Store_Category.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Permissions;
+using System.Web;
+
+namespace Project.Models
+{
+    public class Store_Category
+    {
+        public int MaSP { get; set; }   
+        public int MaLoai { get; set; }
+        public string TenSP { get; set; }
+        public string Hinh { get; set; }
+        public string DanhMuc { get; set; }
+        public decimal GiaBan { get; set; }
+        public decimal GiamGia { get; set; }
+        public int SoLuongLike { get; set; }
+    }
+}
