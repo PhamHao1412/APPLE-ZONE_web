fa57701964cf537fd21c8f416643251a2c3fc654
KhaiLe02 <khailehi0@gmail.com>
2023-04-25 10:46:31 +0700

    khailehuynh branch

--- Project/Project/Controllers/AdminController.cs
+++ Project/Project/Controllers/AdminController.cs
@@ -13,5 +13,9 @@ namespace Project.Controllers
         {
             return View();
         }
+        public ActionResult Add_Products()
+        {
+            return View();
+        }
     }
 }
